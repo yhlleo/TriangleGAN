4293e03927ea454667e50e6924636a23d82275d1
yhlleo <yahui.cvrs@gmail.com>
2019-07-15 14:43:49 +0200

    update gesturegan

--- models/gesturegan_model.py
+++ models/gesturegan_model.py
@@ -1,16 +1,15 @@
-# Author: Yahui Liu <yahui.liu@unitn.it>
-
 import torch
 import itertools
 import torch.nn.functional as F
 import numpy as np
 from util.image_pool import ImagePool
 from .base_model import BaseModel
+from torchvision.models import vgg16
 
 from . import networks
 from . import gesturegan_networks as gesturegan
 
-class GestureGANModel(BaseModel):
+class GestureGANRawModel(BaseModel):
     """
     GestureGAN
     """
@@ -45,6 +44,7 @@ class GestureGANModel(BaseModel):
         # specify the training losses you want to print out. The training/test scripts will call <BaseModel.get_current_losses>
         self.loss_names = ['D', 'GAB', 'rec', 'cycle', 'idt', 'color']
         # specify the images you want to save/display. The training/test scripts will call <BaseModel.get_current_visuals>
+        #self.visual_names = ['real_A', 'cond_B', 'fake_B', ]  # combine visualizations for A
         self.visual_names = ['real_B', 'fake_B']
         # specify the models you want to save to the disk. The training/test scripts will call <BaseModel.save_networks> and <BaseModel.load_networks>.
         if self.isTrain:
@@ -53,14 +53,14 @@ class GestureGANModel(BaseModel):
             self.model_names = ['G']
 
         # define networks (both Generators and discriminators)
-        self.netG = gesturegan.define_GGesture(opt.input_nc+opt.cond_dim,
+        self.netG = gesturegan.define_GGesture(opt.input_nc+opt.cond_dim, # (3+1)x256x256
                                                opt.output_nc,
                                                opt.ngf,
                                                opt.norm,
                                                not opt.no_dropout,
                                                opt.init_type,
                                                opt.init_gain,
-                                               self.gpu_ids)
+                                               self.gpu_ids)  # out_nc: 3x256x256
 
         if self.isTrain:  # define discriminators
             self.netD = gesturegan.define_DGesture(opt.output_nc, 
@@ -78,6 +78,10 @@ class GestureGANModel(BaseModel):
             self.criterionGAN = networks.GANLoss(opt.gan_mode).to(self.device)  # define GAN loss.
             self.criterionRec1 = torch.nn.L1Loss()
             self.criterionRec2 = torch.nn.MSELoss()
+
+            self.vgg_mdoel = vgg16(pretrained=True).to(self.device).cuda()
+            self._set_parameter_requires_grad(self.vgg_mdoel, True)
+            self.vgg_mdoel.eval()
 
             # initialize optimizers; schedulers will be automatically created by function <BaseModel.setup>.
             self.optimizer_G = torch.optim.Adam(self.netG.parameters(), 
@@ -101,6 +105,11 @@ class GestureGANModel(BaseModel):
         #else:
         self.image_paths = input['A_paths']
 
+    def _set_parameter_requires_grad(self, model, feature_extracting):
+        if feature_extracting:
+            for param in model.parameters():
+                param.requires_grad = False
+
     def _channel_loss(self, x, y):
         return self.criterionRec1(x[:,0,:,:], y[:,0,:,:]) + self.criterionRec2(x[:,0,:,:], y[:,0,:,:]) + \
                self.criterionRec1(x[:,1,:,:], y[:,1,:,:]) + self.criterionRec2(x[:,1,:,:], y[:,1,:,:]) + \
@@ -118,8 +127,14 @@ class GestureGANModel(BaseModel):
             self.cycle_A = self.netG(torch.cat([self.fake_B, self.cond_A],dim=1))
             
             # identity: G(A, S_A) -> A and G(B, S_B) -> B
-            self.idt_A = self.netG(torch.cat([self.real_A, self.cond_A],dim=1))
-            self.idt_B = self.netG(torch.cat([self.real_B, self.cond_B],dim=1))
+            #self.idt_A = self.netG(torch.cat([self.real_A, self.cond_A],dim=1))
+            #self.idt_B = self.netG(torch.cat([self.real_B, self.cond_B],dim=1))
+
+            self.feature_real_B = self.vgg_mdoel(F.interpolate(self.real_B, size=(224, 224), mode='bilinear', align_corners=False))
+            self.feature_fake_B = self.vgg_mdoel(F.interpolate(self.fake_B, size=(224, 224), mode='bilinear', align_corners=False))
+
+            self.feature_real_A = self.vgg_mdoel(F.interpolate(self.real_A, size=(224, 224), mode='bilinear', align_corners=False))
+            self.feature_fake_A = self.vgg_mdoel(F.interpolate(self.fake_A, size=(224, 224), mode='bilinear', align_corners=False))
 
     def backward_D(self):
         """Calculate the loss for Discriminator D"""
@@ -134,7 +149,7 @@ class GestureGANModel(BaseModel):
         fake_A = self.fake_A_pool.query(self.fake_A)
         loss_D_fake_A = self.criterionGAN(self.netD(self.fake_A.detach()), False)
         
-        self.loss_D = (loss_D_real_A + loss_D_fake_A + loss_D_real_B + loss_D_fake_B) *  0.5
+        self.loss_D = (loss_D_real_A + loss_D_fake_A + loss_D_real_B + loss_D_fake_B)
         self.loss_D.backward()
 
     def backward_G(self):
@@ -142,27 +157,28 @@ class GestureGANModel(BaseModel):
         # GAN loss D(G(*))
         loss_G_B = self.criterionGAN(self.netD(self.fake_B), True)
         loss_G_A = self.criterionGAN(self.netD(self.fake_A), True)
-        self.loss_GAB = (loss_G_A + loss_G_B) * 0.5 * self.opt.lambda_adv
+        self.loss_GAB = (loss_G_B + loss_G_A) * self.opt.lambda_adv
 
         # forward reconstruction loss
         loss_rec_B = self.criterionRec1(self.real_B, self.fake_B) + self.criterionRec2(self.real_B, self.fake_B)
         loss_rec_A = self.criterionRec1(self.real_A, self.fake_A) + self.criterionRec2(self.real_A, self.fake_A)
-        self.loss_rec = (loss_rec_A + loss_rec_B) * 0.5 * self.opt.lambda_rec
+        self.loss_rec = (loss_rec_B + loss_rec_A) * self.opt.lambda_rec
 
         # cycle reconstruction loss
-        loss_cycle_B = self.criterionRec1(self.real_B, self.cycle_B)
         loss_cycle_A = self.criterionRec1(self.real_A, self.cycle_A)
-        self.loss_cycle = (loss_cycle_A + loss_cycle_B) * 0.5 * self.opt.lambda_cycle
+        loss_cycle_B = self.criterionRec1(self.real_B, self.cycle_B)
+        self.loss_cycle = (loss_cycle_A + loss_cycle_B) * self.opt.lambda_cycle
 
         # self-reconstruction (identity) loss
-        loss_idt_B = self.criterionRec1(self.real_B, self.idt_B)
-        loss_idt_A = self.criterionRec1(self.real_A, self.idt_A)
-        self.loss_idt = (loss_idt_A + loss_idt_B) * 0.5 * self.opt.lambda_idt
+        loss_idt_B = self.criterionRec1(self.feature_real_B, self.feature_fake_B)
+        loss_idt_A = self.criterionRec1(self.feature_real_A, self.feature_fake_A)
+        #loss_idt_A = self.criterionRec1(self.real_A, self.idt_A)
+        self.loss_idt = (loss_idt_B + loss_idt_A) * self.opt.lambda_idt
 
         # color loss
         loss_color_B = self._channel_loss(self.real_B, self.fake_B)
         loss_color_A = self._channel_loss(self.real_A, self.fake_A)
-        self.loss_color = (loss_color_A + loss_color_B) * 0.5 * self.opt.lambda_color
+        self.loss_color = (loss_color_B + loss_color_A)* self.opt.lambda_color
 
         self.loss_G = self.loss_GAB + self.loss_rec + self.loss_cycle + self.loss_idt + self.loss_color
         self.loss_G.backward()
